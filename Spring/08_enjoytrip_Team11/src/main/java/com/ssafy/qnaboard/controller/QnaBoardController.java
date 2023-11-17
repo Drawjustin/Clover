844641d0791dda469c256a094e51bc9b9ba014f1
전새벽 <wjswwkd@naver.com>
2023-11-17 17:49:58 +0900

    my_first_branch^^
    Former-commit-id: c22609f708bd48186ce5f713e162058adaf3a5fd

--- Spring/08_enjoytrip_Team11/src/main/java/com/ssafy/qnaboard/controller/QnaBoardController.java
+++ Spring/08_enjoytrip_Team11/src/main/java/com/ssafy/qnaboard/controller/QnaBoardController.java
@@ -1,5 +1,3 @@
-package com.ssafy.qnaboard.controller;
-
-public class QnaBoardController {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:cb33137c68c75ed5c7f05fc0761c43213cc9077f2e2b4d7db227b7a1b225f386
+size 77
