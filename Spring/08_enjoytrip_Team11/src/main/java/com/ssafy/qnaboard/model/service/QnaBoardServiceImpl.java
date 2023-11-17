4821873ef0ce6d7da2df63e3d3d7a4f5dabd97a7
조은별 <joeunbyeol98@gmail.com>
2023-11-17 17:51:27 +0900

    feat. qna board 작업중
    Former-commit-id: e0d3458a4309f8816e2b0ba3db5e380f35a91e5f

--- Spring/08_enjoytrip_Team11/src/main/java/com/ssafy/qnaboard/model/service/QnaBoardServiceImpl.java
+++ Spring/08_enjoytrip_Team11/src/main/java/com/ssafy/qnaboard/model/service/QnaBoardServiceImpl.java
@@ -1,7 +1,3 @@
-package com.ssafy.qnaboard.model.service;
-import org.springframework.stereotype.Service;
-
-@Service
-public class QnaBoardServiceImpl {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:d7b65fbae3add267d84f8d7e62fe0afbff4ac0dfe247435a819528941212112a
+size 2510
