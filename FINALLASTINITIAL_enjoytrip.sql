c7a1867e8819191e2e71a85119702bbc85d47a19
조은별 <joeunbyeol98@gmail.com>
2023-11-17 11:12:43 +0900

    db 수정
    Former-commit-id: d9ec3a9f1535aff009c5a35b8cf68dfcfde0a948

--- FINALLASTINITIAL_enjoytrip.sql
+++ FINALLASTINITIAL_enjoytrip.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb55d7eca38c135f69c3a902d9ddbd91eff39df4edf1f18a119189f32688324e
-size 10195
+oid sha256:1636ab03f2f6b83c494721f66059fed4cda2ee32a8efb3604a7831e2f31bff68
+size 11268
