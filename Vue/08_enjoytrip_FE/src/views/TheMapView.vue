3f1f2f250a3eb0d0f976155d87e4713218b76165
배성준 <bdhbsj@gmail.com>
2023-11-20 10:38:23 +0900

    fix. checkbox 선택 안했을때 발생하는 에러 해결
    Former-commit-id: 1c3d9c31cf154fe31516fdee26c84f1393fe5056

--- Vue/08_enjoytrip_FE/src/views/TheMapView.vue
+++ Vue/08_enjoytrip_FE/src/views/TheMapView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c2a4e3ef5768c940bfe21723db173ef036f64d5582c8917142d7cd63964947b
-size 6505
+oid sha256:e8a180656a7ce592f3892b64f0109e5ebbe19cbd924d0a73cdae895d910e69d6
+size 6625
