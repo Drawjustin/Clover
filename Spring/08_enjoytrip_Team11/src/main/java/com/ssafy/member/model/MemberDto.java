3c7e523073a090be5e04043ff2beae49dee80b4f
woori <woori50300@gmail.com>
2023-11-20 12:15:34 +0900

    test:member Branch
    Former-commit-id: 4a3254dc43311d75b8d812e53f65f701c7154529

--- Spring/08_enjoytrip_Team11/src/main/java/com/ssafy/member/model/MemberDto.java
+++ Spring/08_enjoytrip_Team11/src/main/java/com/ssafy/member/model/MemberDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d96541d28115acadf7dc84217d1f4f542f866bc0a94715d22acf7e10914ff275
-size 1106
+oid sha256:195727f89a00e07c806c2cde8acbbff989e58fa0d57d63990cd2874116a62fa5
+size 218
--- Spring/08_enjoytrip_Team11/src/main/java/com/ssafy/member/model/MemberModifyDto.java
+++ Spring/08_enjoytrip_Team11/src/main/java/com/ssafy/member/model/MemberModifyDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:919d0e206af845f4ade4a15429b53cf20416739a06f79c81c923a26aaa6f28ef
-size 838
+oid sha256:7ce75fb01523a80fde796c6ed2bd4968fbfbdcddcb6e18d01e01181874d4b7ef
+size 835
