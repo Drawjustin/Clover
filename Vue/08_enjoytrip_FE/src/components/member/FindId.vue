67af8d6ff921b77dd472b0c3f5f91ade7c765419
woori <woori50300@gmail.com>
2023-11-22 09:29:07 +0900

    add: 아이디찾기, 비밀번호 찾기 폼
    Former-commit-id: 2c7f49a00d40e6e73b3f034028c96878cb5bf3d1

--- Vue/08_enjoytrip_FE/src/components/member/FindId.vue
+++ Vue/08_enjoytrip_FE/src/components/member/FindId.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:208066b24f7a439cf550f06a1bb3da9b679cd9cbac5339eab6550c3dd747875a
-size 2652
+oid sha256:e0b3da0ed4ed3d6b45b7b87fd51378283d7de4c944b180c817a6b782d19b2b0e
+size 3663
--- Vue/08_enjoytrip_FE/src/components/member/FindPass.vue
+++ Vue/08_enjoytrip_FE/src/components/member/FindPass.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c452035db711402c837d8fe0866b5b0ad5b6423b8108583e6029dacde7eaa0f7
-size 2642
+oid sha256:d23981c631cb68f1c20c237ea266e86ba33ba7a06806bc129e01bfe0ed9fb94d
+size 4199
