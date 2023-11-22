8b76aaec471d204a937f3fc7147b0168ee8997e0
전새벽 <wjswwkd@naver.com>
2023-11-22 15:09:04 +0900

    email-login-mypage finish
    Former-commit-id: 81a3a7e94d3830a0903f440e49aae1dd83b224e5

--- Vue/08_enjoytrip_FE/src/views/UserInfoPage.vue
+++ Vue/08_enjoytrip_FE/src/views/UserInfoPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:446def5093b904b3d764045fbc4d1cac9701b31e753d691766af0ddb97f3afea
-size 5514
+oid sha256:6b0f3237911a1aec0417321013557044b4d3e8807f36b1317a109e55e18fe25e
+size 5694
