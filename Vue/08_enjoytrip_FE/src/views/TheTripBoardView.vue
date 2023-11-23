c8fc67e8a0f9183ab876e190527141c13d8c7732
woori <woori50300@gmail.com>
2023-11-23 22:02:10 +0900

    modify: title색상변경
    Former-commit-id: 419971d909c2d5ffe368e728f1435813d58eda51

--- Vue/08_enjoytrip_FE/src/views/TheTripBoardView.vue
+++ Vue/08_enjoytrip_FE/src/views/TheTripBoardView.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="container text-center mt-3">
-    <div class="alert alert-info" role="alert">여행 기록 남기는 공간</div>
+    <div class="alert" role="alert">여행 기록 남기는 공간</div>
     <router-view></router-view>
   </div>
 </template>
