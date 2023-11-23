b72fbd19cb8daf21ee2df8193c60f939de8f0c03
BBSSJJ <bdhbsj@naver.com>
2023-11-23 16:25:58 +0900

    tmp change
    Former-commit-id: 975e10b0e93e45a1b5bacedf7d61dbdc72d4d425

--- Vue/08_enjoytrip_FE/src/components/tripboard/item/TripBoardFormItem.vue
+++ Vue/08_enjoytrip_FE/src/components/tripboard/item/TripBoardFormItem.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb0558c0f4079aee2780ec9b0a18d82c2e5c49428f1b4180d0777b2e72fbccc4
-size 10748
+oid sha256:d88fac8fde902cec930aaa9c45322131891c97446273e9b3fada895dc0b345ac
+size 10719
