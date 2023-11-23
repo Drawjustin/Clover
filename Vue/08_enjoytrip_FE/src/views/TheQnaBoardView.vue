7f54b1954f2c894f86a743f37e4210fc96a68780
유현종 <guswhd903@naver.com>
2023-11-23 15:10:00 +0900

    Revert "final Merge"
    This reverts commit b0464a7a2388c24645c83aab842ae37302d149dc [formerly 8a27dc573a7ab40d0c008beb31f1acae8a3fdad8]
    
    Former-commit-id: 337142af131574c085df640850317fd94030b31f

--- Vue/08_enjoytrip_FE/src/views/TheQnaBoardView.vue
+++ Vue/08_enjoytrip_FE/src/views/TheQnaBoardView.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="container text-center mt-3">
-    <div class="alert alert-info" role="alert">질문하는 공간</div>
+    <div role="alert">자유롭게 글쓰는 공간</div>
     <router-view></router-view>
   </div>
 </template>
