3634cd202a026c122f27679a29543043b871482a
woori <woori50300@gmail.com>
2023-11-23 14:08:12 +0900

    modify: css layout
    Former-commit-id: 17300075965669eb82f516aaa781461b71798918

--- Vue/08_enjoytrip_FE/src/components/layout/TheHeadingNavbar.vue
+++ Vue/08_enjoytrip_FE/src/components/layout/TheHeadingNavbar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32ed77375399f8ebedc8f0fe832418814ebc3769ea831d20db3054fb8a24db4a
-size 2797
+oid sha256:16e84a7e9a333cb173332f46f0cc24e9a44a451f871776ad3313710a08bebc78
+size 2820
--- Vue/08_enjoytrip_FE/src/components/member/FindPass.vue
+++ Vue/08_enjoytrip_FE/src/components/member/FindPass.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fea9343ede2f308aaeccbe666139061466a71d86b85964873fdff6a9d07ea12f
-size 4392
+oid sha256:91627a991bddb005600c9ff77b54fb449ff4f3398df20ebd7f80e6dfff20f8e7
+size 4285
