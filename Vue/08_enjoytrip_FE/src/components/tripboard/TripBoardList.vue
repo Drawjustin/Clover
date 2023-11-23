ed78347a0fd76f21176d3da79d626f82655fe143
woori <woori50300@gmail.com>
2023-11-24 00:06:06 +0900

    css: form구현
    Former-commit-id: 53a863ffda94e2441d2c83e151f740f52b257a91

--- Vue/08_enjoytrip_FE/src/components/tripboard/TripBoardList.vue
+++ Vue/08_enjoytrip_FE/src/components/tripboard/TripBoardList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eec99398f0841d0ab9baa3715364a290192370ca10fd2d35bbc88ba5eaf15be3
-size 3481
+oid sha256:36dbaf6eb27f5aabf4b29fad4655d0504c491aea597a5547914e87a53ae22e41
+size 3530
