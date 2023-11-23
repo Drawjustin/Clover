0bc7c5efb380ac1d5ef158cdb72a560e95107f37
유현종 <guswhd903@naver.com>
2023-11-23 15:11:37 +0900

    Revert "modify: css layout"
    This reverts commit 9c7bd9b892eb369425508f361913839c828e67e8 [formerly b9b5fd44c8f811d155f3f46ef419fcb79ee1ae7c]
    
    Former-commit-id: 47b9189767700233de774fbea527c0de160e4db2

--- Vue/08_enjoytrip_FE/src/views/UserInfoPage.vue
+++ Vue/08_enjoytrip_FE/src/views/UserInfoPage.vue
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:984cbac057b08c136fc7c49bbc14255c9c8fff0217bd9ba8e1bd6382d4f03d94
-size 121
+<script setup></script>
+
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped></style>
