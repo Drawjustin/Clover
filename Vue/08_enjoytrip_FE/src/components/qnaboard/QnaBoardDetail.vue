a09de87e968f37d34a8435905c30640ba16f77b2
Drawjustin <guswhd903@naver.com>
2023-11-23 22:50:19 +0900

    feature-qna
    Former-commit-id: 42f3ced55a0355ba7fda1b7e7317680449de46f2

--- Vue/08_enjoytrip_FE/src/components/qnaboard/QnaBoardDetail.vue
+++ Vue/08_enjoytrip_FE/src/components/qnaboard/QnaBoardDetail.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e39b1e241f118d29c7ac21bf81fb96a47c43d5d0b662e03706314c0230adc98d
-size 3573
+oid sha256:f7dd0d11a7f289aaa5e8393fcef02022464b27f84029d3de495b33272228411c
+size 3804
