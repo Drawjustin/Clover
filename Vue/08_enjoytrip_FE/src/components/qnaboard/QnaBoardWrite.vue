deaa95db7c344a6476f109425696ec7c5d0a81d1
woori <woori50300@gmail.com>
2023-11-23 17:05:48 +0900

    11/23 05:05
    Former-commit-id: 0156c6ecdafed81022da3ef3944e89d86f32a4de

--- Vue/08_enjoytrip_FE/src/components/qnaboard/QnaBoardWrite.vue
+++ Vue/08_enjoytrip_FE/src/components/qnaboard/QnaBoardWrite.vue
@@ -1,3 +1,20 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:05655c4499dad9f89e0da862342af8c98fdfb4601050b51d7131520b6c41a828
-size 485
+<script setup>
+import QnaBoardFormItem from './item/QnaBoardFormItem.vue';
+</script>
+
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-10">
+        <h2 class="my-3 py-3 shadow-sm bg-light text-center">
+          <mark class="sky">글쓰기</mark>
+        </h2>
+      </div>
+      <div class="col-lg-10 text-start">
+        <QnaBoardFormItem type="regist" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
